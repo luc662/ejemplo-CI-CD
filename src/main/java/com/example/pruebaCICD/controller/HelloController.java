b33323f879755895f046db3abc6c91a426d9c5e7
lucas bonfil <lbonfil@fi.uba.ar>
2024-10-25 14:23:42 -0300

    created an endpoint

---
+++ src/main/java/com/example/pruebaCICD/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.pruebaCICD;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "¡Hola, mundo!";
+    }
+}
